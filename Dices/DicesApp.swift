ba2be924c9b6cf413c4540e72b94c7ed6fc61e1d
ZavNav <arka146@mail.ru>
2022-03-16 20:11:41 +0300

    Initial Commit

---
+++ Dices/DicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DicesApp.swift
+//  Dices
+//
+//  Created by Аркадий Рожков on 16.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct DicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
